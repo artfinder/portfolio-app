469a297987bc075ad27c6a3c0d6002653323b6b9
Dawid Lorenz <dawid@lorenz.co>
2014-09-30 14:51:17 +0200

    Removed unnecessary stuff from app.js

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -26,14 +26,14 @@ angular.module('portfolio', [
 
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+    // if (window.cordova && window.cordova.plugins.Keyboard) {
+    //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    // }
 
     // iOS statusbar tweaks
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-      StatusBar.overlaysWebView(true);
-    }
+    // if (window.StatusBar) {
+    //   StatusBar.styleDefault();
+    //   StatusBar.overlaysWebView(true);
+    // }
   });
 });
